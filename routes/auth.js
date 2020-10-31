1f907e26f355da7171b87e55c204e515e0508e09
Henrik Frank <frank.havsvinge@gmail.com>
2020-10-31 21:35:15 +0100

    Implemented login function

--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,27 @@ const router = express.Router()
 const mySql = require('../database/mysql')
 const jwt = require('jsonwebtoken')
 
-router.get('/login', (req, res) => {
-  res.send({ msg: 'logged in' })
+router.post('/login', (req, res) => {
+  mySql.query('SELECT * FROM users WHERE userName=?', [req.body.userName], (err, users) => {
+    if (err) throw err
+
+    if (users.length === 0) {
+      return res.send({ status: 2, msg: 'Incorrect username or password' })
+    }
+
+    if (users[0].userPassword === req.body.userPassword) {
+      const token = jwt.sign({ userName: users[0].userName, userId: users[0].userId }, process.env.JWT_SECRET, { expiresIn: '7d' })
+
+      delete users[0].userPassword
+
+      return res.send({ status: 1, msg: 'Logged in!', token, user: users[0] })
+    } else {
+      return res.send({ status: 2, msg: 'Incorrect username or password' })
+    }
+  })
 })
 
 router.post('/signup', (req, res) => {
-
   if (req.body.userName == undefined || req.body.userPassword == undefined) {
     return res.send({ status: 3, msg: 'Fill out username and password' })
   }
@@ -27,7 +42,7 @@ router.post('/signup', (req, res) => {
     mySql.query('INSERT INTO users (userName, userPassword) VALUES (?, ?)', [req.body.userName, req.body.userPassword], (err) => {
       if (err) throw err
 
-      return res.send({ msg: 'Done' })
+      return res.send({ status: 1, msg: 'User created!' })
     })
   })
 })
--- routes/chat.js
+++ routes/chat.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const { authUser } = require('../middleware/middleware')
 
 router.get('/messages', authUser, (req, res) => {
-  res.send([{msg: 'hej'}, {msg: 'hallå'}])
+  res.send([{msg: 'hej ' + req.user.userName }, {msg: 'ditt id är ' + req.user.userId }])
 })
 
 module.exports = router
